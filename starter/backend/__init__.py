d9e4d10ed3854e14f1498086962ad9b2f3844d20
Duvvuru Akshay Reddy <akshayreddy511@gmail.com>
2026-09-03 16:31:34 +0530

    Python version change

--- starter/backend/__init__.py
+++ starter/backend/__init__.py
@@ -1,14 +1,15 @@
 import os
 from flask import Flask
 from flask_cors import CORS
-
 from .movies import movies_api
 
 app = Flask(__name__)
-CORS(app)
+
+# Explicitly allow all origins and credentials for all routes including blueprints
+CORS(app, resources={r"/*": {"origins": "*"}}, supports_credentials=True)
+
 app.register_blueprint(movies_api)
 
-# Start app
 if __name__ == "__main__":
     app.run(
         debug=True,
