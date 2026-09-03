cabfdee887ddf4029338305619d60944041fdd9a
Duvvuru Akshay Reddy <akshayreddy511@gmail.com>
2026-09-03 12:22:41 +0530

    Fix workflow triggers for CI and CD

--- setup/terraform/variables.tf
+++ setup/terraform/variables.tf
@@ -1,19 +1,24 @@
-variable "k8s_version" {
-  default = "1.25"
-}
-
-variable "enable_private" {
-  default = false
+variable "cluster_name" {
+  type    = string
+  default = "cluster"
 }
 
 variable "public_az" {
-  type        = string
-  description = "Change this to a letter a-f only if you encounter an error during setup"
-  default     = "a"
+  type    = string
+  default = "a"
 }
 
 variable "private_az" {
-  type        = string
-  description = "Change this to a letter a-f only if you encounter an error during setup"
-  default     = "b"
+  type    = string
+  default = "b"
+}
+
+variable "k8s_version" {
+  type    = string
+  default = "1.33"
+}
+
+variable "enable_private" {
+  type    = bool
+  default = false
 }
